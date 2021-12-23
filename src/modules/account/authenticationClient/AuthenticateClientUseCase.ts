8aaab0f11078e94280a78f7f1eaa0a8ac5d8ea5d
joaodvilla <joaodvilla@gmail.com>
2021-12-23 17:13:24 -0300

    feat: create use case to authenticate

---
+++ src/modules/account/authenticationClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { prisma } from "../../../database/prismaClient";
+
+// receber username e password
+// verificar se o cliente existe
+// verificar se a senha está correta
+// gerar token
+
+
+interface IAuthenticateClient {
+  username: string;
+  password: string;
+};
+
+export class AuthenticateClientUseCase {
+  async execute({ username, password }: IAuthenticateClient) {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username
+      }
+    });
+
+    if (!client) {
+      throw new Error('Invalid username or password');
+    }
+
+    const passwordIsValid = await bcrypt.compare(password, client.password);
+
+    if (!passwordIsValid) {
+      throw new Error('Invalid username or password');
+    }
+
+    const token = sign({username}, process.env.SECRET_KEY as string, {
+      subject: client.id,
+      expiresIn: '1d',
+    });
+
+    return token;
+  }
+}
