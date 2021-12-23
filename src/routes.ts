5969a21f7682e3784142ccf90f3e693f724bf7a5
joaodvilla <joaodvilla@gmail.com>
2021-12-23 17:19:44 -0300

    feat: add authenticate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/authenticationClient/AuthenticataClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
-routes.post("/clients", createClientController.handle)
+routes.post("/clients", createClientController.handle);
+routes.post("/login", authenticateClientController.handle);
+
 export { routes };
