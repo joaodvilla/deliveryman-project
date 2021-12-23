afaba25e9b519baa3014408d51a29b54af5beab4
joaodvilla <joaodvilla@gmail.com>
2021-12-23 16:52:26 -0300

    feat: create use case to create user client

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,35 @@
+import bcrypt from 'bcrypt';
+import { prisma } from '../../../../database/prismaClient';
+
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient): Promise<ICreateClient> {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        }
+      },
+    });
+
+    if (clientExists) {
+      throw new Error('Client already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    return client;
+  }
+}
