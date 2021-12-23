23aba3c895b88818f8221de8f589c6091d39544a
joaodvilla <joaodvilla@gmail.com>
2021-12-23 17:28:16 -0300

    feat: add error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 500,
+    message: "Internal server error",
+  });
+})
 
 app.listen(3000, () => console.log("Server started on port 3000"));
