c46dad886d78a4d01714ec5b0e5a98508c6b2acc
joaodvilla <joaodvilla@gmail.com>
2021-12-28 17:29:46 -0300

    feat: create use case to create user deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,35 @@
+
+import bcrypt from 'bcrypt';
+import { prisma } from '../../../../database/prismaClient';
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+};
+
+export class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreateDeliveryman): Promise<ICreateDeliveryman> {
+    const deliverymanExists = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        }
+      },
+    });
+
+    if (deliverymanExists) {
+      throw new Error('Deliveryman already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    return deliveryman;
+  }
+}
