6272a7bb0a732f316b51f1d1aa4b16e326093c6e
joaodvilla <joaodvilla@gmail.com>
2021-12-29 00:20:47 -0300

    feat: add route to deliveryman accept delivery

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
+import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
 import { AuthenticateClientController } from "./modules/account/authenticationClient/AuthenticataClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticationDeliveryman/AuthenticataDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
+import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createDeliveryController = new CreateDeliveryController();
+const updateDeliveryController = new UpdateDeliverymanController();
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const findAllAvailableController = new FindAllAvailableController();
@@ -23,6 +26,7 @@ routes.post("/login-client", authenticateClientController.handle);
 routes.post("/login-deliveryman", authenticateDeliverymanController.handle);
 
 routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle);
-routes.get("/delivery", findAllAvailableController.handle);
+routes.put("/delivery/:id", ensureAuthenticateDeliveryman, updateDeliveryController.handle);
+routes.get("/delivery", ensureAuthenticateDeliveryman, findAllAvailableController.handle);
 
 export { routes };
