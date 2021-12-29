80886e18959a236eedbdcce8dfabd03c8386ed97
joaodvilla <joaodvilla@gmail.com>
2021-12-28 23:49:52 -0300

    refactor: alter folder architecture

---
+++ src/modules/deliveries/useCases/createDelivery/CreateDeliveryUseCase.ts
@@ -0,0 +1,19 @@
+import { prisma } from '../../../../database/prismaClient';
+
+interface ICreateDelivery {
+  itemName: string;
+  idClient: string;
+}
+
+export class CreateDeliveryUseCase {
+  async execute({itemName, idClient}: ICreateDelivery) {
+    const delivery = await prisma.deliveries.create({
+      data: {
+        itemName,
+        idClient,
+      }
+    });
+
+    return delivery;
+  }
+}
