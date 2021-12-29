e494640af6ba18ce421c500c2d30848a74089424
joaodvilla <joaodvilla@gmail.com>
2021-12-28 23:25:12 -0300

    feat: add delivery with middleware route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
 import { AuthenticateClientController } from "./modules/account/authenticationClient/AuthenticataClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticationDeliveryman/AuthenticataDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
+const createDeliveryController = new CreateDeliveryController();
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateClientController = new AuthenticateClientController();
@@ -13,7 +16,10 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 
 routes.post("/clients", createClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
+
 routes.post("/login-client", authenticateClientController.handle);
 routes.post("/login-deliveryman", authenticateDeliverymanController.handle);
+
+routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle);
 
 export { routes };
